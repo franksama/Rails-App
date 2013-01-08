7a6a14fbc32d190fece4eb1cf27f284d8f687128
Frank Huang <fhuang@homeaway.com>
2013-01-08 14:06:54 -0600

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,7 +33,10 @@ describe "Authentication" do
             it { should have_link('Profile', :href => user_path(user)) }
             it { should have_link('Settings', :href => edit_user_path(user)) }
             it { should have_link('Sign out', :href => signout_path) }
+            it { should have_link('Users', :href => users_path) }
+        
             it { should_not have_link('Sign in', :href => signin_path) }
+
 
             describe "followed by signout" do
                 before { click_link "Sign out" }
@@ -49,6 +52,7 @@ describe "Authentication" do
     end
 
     describe "authorization" do
+
         describe "for non-signed-in users" do
             let(:user) { FactoryGirl.create(:user) }
 
@@ -62,6 +66,11 @@ describe "Authentication" do
                 describe "submitting to the update action" do
                     before { put user_path(user) }
                     specify { response.should redirect_to(signin_path) }
+                end
+
+                describe "visiting the user index" do
+                    before { visit users_path }
+                    it { should have_selector('title', :text => 'Sign in') }
                 end
             end
 
@@ -94,6 +103,18 @@ describe "Authentication" do
 
             describe "submitting a PUT request to the Users#update action" do
                 before { put user_path(wrong_user) }
+                specify { response.should redirect_to(root_path) }
+            end
+        end
+
+        describe "as non-admin user" do
+            let(:user) { FactoryGirl.create(:user) }
+            let(:non_admin) { FactoryGirl.create(:user) }
+
+            before { sign_in non_admin }
+
+            describe "submitting a DELETE request to the Users#destroy action" do
+                before { delete user_path(user) }
                 specify { response.should redirect_to(root_path) }
             end
         end
