21665581581042eeaac7f1e8913d9f7b499232aa
Frank Huang <fhuang@homeaway.com>
2013-01-10 10:09:16 -0600

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,12 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, :length => { :minimum => 6 }
   validates :password_confirmation, :presence => true
 
+  has_many :microposts, :dependent => :destroy
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     def create_remember_token
       self.remember_token = SecureRandom.hex
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,24 @@ describe "StaticPages" do
 
         it { should have_selector('h1', :text => 'Sample App') }
         it { should have_selector('title', :text => full_title('')) }
+
+        describe "for signed-in users" do
+
+            let(:user) { FactoryGirl.create(:user) }
+            
+            before do
+                FactoryGirl.create(:micropost, :user => user, :content => "Lorem ipsum")
+                FactoryGirl.create(:micropost, :user => user, :content => "Dolor sit amet")
+            end
+
+            it "should render the user's feed" do
+                user.feed.each do |item|
+                    page.should have_selector("li##{item.id}", :text => item.content)
+                end
+            end
+
+        end
+
     end
 
     describe "Help page" do
@@ -45,5 +63,23 @@ describe "StaticPages" do
             visit contact_path
             page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Contact')
         end
+    end
+
+    describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+        let!(:m1) { FactoryGirl.create(:micropost, :user => user, :content => "Foo") }
+        let!(:m2) { FactoryGirl.create(:micropost, :user => user, :content => "Bar") }
+
+        before { visit user_path(user) }
+
+        it { should have_selector('h1', :text => user.name) }
+        it { should have_selector('title', :text => user.name) }
+
+        describe "microposts" do
+            it { should have_content(m1.content) }
+            it { should have_content(m2.content) }
+            it { should have_content(user.microposts.count) }
+        end
+
     end
 end
